09c885a60da380d531acfe953cc96c714b88ff8e
Christianpereira85 <38117481+Christianpereira85@users.noreply.github.com>
2022-05-16 21:36:34 -0300

    Add files via upload

---
+++ proy.java
@@ -0,0 +1,48 @@
+package proyecto;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class proy {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					proy window = new proy();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public proy() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("CLIC");
+		btnNewButton.setBounds(31, 101, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
